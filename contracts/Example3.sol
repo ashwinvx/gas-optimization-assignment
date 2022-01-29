a0f8a6b458ca48df3e6a4e7cd45fd6d70b49af79
ashwinvx@gmail.com <ashwinvx@gmail.com>
2022-01-29 09:23:51 -0500

    Example 3 update

--- contracts/Example3.sol
+++ contracts/Example3.sol
@@ -1,21 +1,23 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-/// @title A contract to show gas optimization by limiting number fo operations
+/// @title A contract to show gas optimization by using pure function and limiting number of operations
 /// @author Ashwin Rachakonda 💯💯😎💯💯
-/// @notice Instead of calculating amounts separately we calculate one and assign the remaining value to the other.
+/// @notice Change function from view to pure and also limit number of operations
+
 
 //Function call gas estimates
 //25065 - before
-//24574 - after
+//24574 - after reducing number of operations
+//22487 - after changing function from view to pure and moving state variable inside the function
 
 contract Example3 {
-    uint256 marginPercentage = 30;
     function getOwnerMargin(uint256 amount)
         public
-        view
+        pure
         returns (uint256 amountForSender, uint256 amountForOwner)
     {
+        uint256 marginPercentage = 30;
         amountForOwner = (amount * marginPercentage) / 100;
         amountForSender = amount - amountForOwner;
     }
