abddf8ce8142112a2389df4087b396b80692ff8b
ashwinvx@gmail.com <ashwinvx@gmail.com>
2022-01-29 09:09:53 -0500

    Gas Optimization assignment - Ashwin Rachakonda

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -1,11 +1,25 @@
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
+
+/// @title A contract to show gas optimization by using temp variables and calldata
+/// @author Ashwin Rachakonda 💯💯😎💯💯
+/// @notice A contract to show gas optimization by using temp variables and calldata
+
+//Function call gas estimates
+//50355 - original
+//48534 - after calldata
+//48499 - after temp variable to keep array length
+//47719 - after using temp variable to calculate sum
 
 contract Example2 {
     uint public counter;
- 
-    function incrementBy(uint[] memory arr) public {
-        for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+    
+    function incrementBy(uint[] calldata arr) public {
+        uint arrLength =  arr.length;
+        uint tempSum = counter;
+        for (uint idx = 0; idx < arrLength; idx++){
+            tempSum += arr[idx];
         }
+        counter += tempSum;
     }
 }
--- contracts/Example3.sol
+++ contracts/Example3.sol
@@ -1,4 +1,13 @@
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
+
+/// @title A contract to show gas optimization by limiting number fo operations
+/// @author Ashwin Rachakonda 💯💯😎💯💯
+/// @notice Instead of calculating amounts separately we calculate one and assign the remaining value to the other.
+
+//Function call gas estimates
+//25065 - before
+//24574 - after
 
 contract Example3 {
     uint256 marginPercentage = 30;
@@ -7,8 +16,7 @@ contract Example3 {
         view
         returns (uint256 amountForSender, uint256 amountForOwner)
     {
-        amountForSender = (amount * (100 - marginPercentage)) / 100;
         amountForOwner = (amount * marginPercentage) / 100;
+        amountForSender = amount - amountForOwner;
     }
-
 }
